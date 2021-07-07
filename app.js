b51674ecc2dfbb411be78b6f61f47b6b15196654
Ali Husni <a.husni@indoleads.com>
2021-07-08 00:07:23 +0800

    renamed controller, added session packages, added authentication wip

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 const cors = require('cors')
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const User = require('./src/models/user');
 
 const feedRoutes = require('./src/routes/web');
 
@@ -22,6 +26,33 @@ app.use((req, res, next) => {
     next();
 });
 
+const store = new MongoDBStore({
+    uri: 'mongodb://mongodb:27017',
+    collection: 'sessions'
+});
+
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store: store
+    })
+);
+
+app.use((req, res, next) => {
+    req.session.isLoggedIn = false;
+    if (!req.session.user) {
+        return next();
+    }
+    User.findById(req.session.user._id)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 let routes = feedRoutes.init();
 app.use(routes);
 
@@ -29,13 +60,13 @@ app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
-    res.status(status).json({ message: message });
+    res.status(status).json({message: message});
 });
 
 mongoose
     .connect(
         'mongodb://mongodb:27017',
-        { useNewUrlParser: true }
+        {useNewUrlParser: true}
     )
     .then(result => {
         app.listen(8080);
